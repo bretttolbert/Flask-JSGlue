3ff62f9f9fca2e9165b5eb842b3a0123d30aa4fa
Brett Tolbert <bretttolbert@gmail.com>
2025-12-06 14:01:45 -0600

    wip

--- flask_jsglue/__init__.py
+++ flask_jsglue/__init__.py
@@ -1,7 +1,12 @@
+from typing import Optional
+
 from flask import Flask
 
 from flask_jsglue.main import bp
 
 
-def init(app: Flask):
-    app.register_blueprint(bp)
+def init(app: Flask, url_prefix: Optional[str] = None):
+    if url_prefix is None:
+        app.register_blueprint(bp)
+    else:
+        app.register_blueprint(bp, url_prefix=url_prefix)
